28c578d2dbfbf8d376d98a5976cddfa7c02873ff
saur0w <sthapliyal085@gmail.com>
2026-08-08 20:06:49 +0530

    feat: add Landing component with GSAP scroll-triggered video animation

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -2,13 +2,28 @@
 
 import styles from "./style.module.scss";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
 import { useRef } from "react";
 
-gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger, useGSAP);
 
 export default function Landing() {
     const landingRef = useRef<HTMLDivElement>(null);
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    useGSAP(() => {
+        gsap.to(videoRef.current, {
+            yPercent: 20,
+            ease: "none",
+            scrollTrigger: {
+                trigger: landingRef.current,
+                start: "top top",
+                end: "bottom top",
+                scrub: true,
+            }
+        })
+    }, { scope: landingRef })
 
     return (
         <section className={styles.landing} ref={landingRef}>
@@ -22,6 +37,7 @@ export default function Landing() {
                     loop
                     muted
                     playsInline
+                    ref={videoRef}
                 />
             </div>
         </section>
